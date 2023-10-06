f9d61cd5b90835d66faab5aaf4419bbf3efd3b07
Ksenix65 <dominikinagawel@gmail.com>
2023-10-06 15:17:36 +0200

    sesja dzialanie

--- data i czas/ciekawostka.php
+++ data i czas/ciekawostka.php
@@ -2,6 +2,6 @@
 $czas = mktime( 11,14,54,8,12,2014 );
 echo "Created date is " .date( "d.m.Y h:i:sa", $czas );
 $now = time();
-$week = 7*25*60*60;
+$week = 7*24*60*60;
 echo "<br> teraz: " .date("d.m.Y h:i:sa" , $now + $week);
 ?>
